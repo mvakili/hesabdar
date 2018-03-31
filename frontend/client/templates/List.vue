b0c94f05539711621bbf596dd394fbd8456b4836
Muhammad Vakili <m.vakili95@gmail.com>
2018-03-31 18:58:57 +0430

    قالب برای صفحات لیستی آماده شد

---
+++ frontend/client/templates/List.vue
@@ -0,0 +1,100 @@
+<template>
+<div>
+  <div class="tile is-ancestor">
+    <div class="tile is-parent is-12">
+      <nav>
+        <slot name="nav">
+        </slot>
+      </nav>
+    </div>
+  </div>
+  <div class="tile is-ancestor">
+    <div class="tile is-parent is-12">
+      <article class="tile is-child box">
+        <section>
+            <b-table
+                :data="table.data"
+                :loading="table.loading"
+                :paginated="table.isPaginated"
+                :per-page="table.perPage"
+                :total="table.total"
+                :current-page.sync="table.currentPage"
+                :backend-sorting="true"
+                :backend-pagination="true"
+                @sort="onSort"
+                @page-change="onPageChange"
+                :mobile-cards="false">
+
+                <template slot-scope="props">
+                  <slot name="table-template" :row="props.row" :index="props.index" >
+                  </slot>
+                </template>
+            </b-table>
+        </section>
+      </article>
+    </div>
+  </div>
+  <slot name="modals">
+  </slot>
+</div>
+</template>
+
+<script>
+
+  export default {
+    data () {
+      return {
+        table: {
+          data: [],
+          total: 0,
+          isPaginated: true,
+          currentPage: 1,
+          perPage: 10,
+          loading: false,
+          sortField: '',
+          sortOrder: 'desc'
+        }
+      }
+    },
+    methods: {
+      onPageChange (page) {
+        this.table.currentPage = page
+        this.$emit('load-data', this.table)
+      },
+      onSort (field, order) {
+        this.table.sortField = field
+        this.table.sortOrder = order
+        this.$emit('load-data', this.table)
+      }
+    },
+    mounted () {
+      this.$emit('load-data', this.table)
+    }
+  }
+</script>
+
+<style>
+.pagination-previous {
+  transform: rotate(180deg);
+}
+.pagination-next {
+  transform: rotate(180deg);
+}
+.table th,
+.table td {
+  text-align: right !important;
+}
+
+.table th:last-child,
+.table td:last-child
+{
+    text-align: left !important;
+}
+.pagination .fa {
+  vertical-align: text-top !important;
+}
+
+.is-desc {
+  transform: rotate(180deg);
+}
+</style>
