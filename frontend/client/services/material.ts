3fcae8510ec2eff0117d1c4f2ed1a97ea9171d74
Muhammad Vakili <m.vakili95@gmail.com>
2018-04-03 07:23:47 +0430

    توابع سرویس Material اضافه شد

--- frontend/client/services/material.ts
+++ frontend/client/services/material.ts
@@ -7,10 +7,17 @@ export default  {
     addMaterial(name: string) : Promise<any>
     {
         return Base.post('material', 'AddMaterial', { Name: name });
-
     },
     getMaterials(page: number, perPage: number, sortField: string, sortOrder: string) : Promise<any>
     {
         return Base.get('material', '?page=' + page + '&&perPage=' + perPage + ((sortField) ? ('&&sort=' + sortField + ' ' + sortOrder) : ''));
+    },
+    getMaterial(id: number) : Promise<any>
+    {
+        return Base.get('material', '' + id);
+    },
+    editMaterial(id: number, material: any) : Promise<any>
+    {
+        return Base.put('material', '' + id , material);
     }
 }
