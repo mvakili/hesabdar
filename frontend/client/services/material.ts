faefc3c9165aafc0bca586b69207e97a12137f1d
Muhammad Vakili <m.vakili95@gmail.com>
2018-04-28 09:15:27 +0430

    Api پیشنهاد کالا با نام

--- frontend/client/services/material.ts
+++ frontend/client/services/material.ts
@@ -19,5 +19,9 @@ export default  {
     edit(id: number, param: any) : Promise<any>
     {
         return Base.put('material', '' + id , param);
+    },
+    suggest(text: string) : Promise<any>
+    {
+        return Base.get('material', 'suggest/' + text);
     }
 }
