82df10b03940119d7a16c17da47fb91a20c06832
Muhammad Vakili <m.vakili95@gmail.com>
2018-05-21 16:45:45 +0430

    #20

--- frontend/client/views/deal/payment/index.vue
+++ frontend/client/views/deal/payment/index.vue
@@ -1,61 +1,68 @@
 <template>
   <div>
     <section>
-      <div class="control is-horizontal">
-        <p class="control">
-          <label class="radio">
-            <b-radio  v-model="deal.dealPayment.method" native-value="Cash" name="paymentType" />
-            نقدی
-          </label>
-          &nbsp;&nbsp;&nbsp;
-          <label class="radio">
-            <b-radio  v-model="deal.dealPayment.method" native-value="Cheque" name="paymentType" />
-            چک
-          </label>
-        </p>
-        <p class="control">
-          <label class="radio">
-            <vb-switch v-model="deal.dealPayment.payed" type="primary" ></vb-switch> &nbsp;
-            <span v-if="deal.dealPayment.payed">
-              پرداخت شده
-            </span>
-            <span v-else>
-              پرداخت نشده
-            </span>
-          </label>
-        </p>
-      </div>
-      <p>&nbsp;</p>
-      <div class="contol" v-if="deal.dealPayment.method == 'Cheque'">
-         <div class="columns">
-          <div  class="column">
-            سررسید چک :
-          </div>
-          <div class="control column is-two-thirds">
-            <date-picker type="date" :auto-submit="true" :editable="true" format="YYYY-MM-DD HH:mm" display-format="jYYYY/jMM/jDD" v-model="deal.dealPayment.dueDate"></date-picker>
-          </div>
-         </div>
-      </div>
-      <p>&nbsp;</p>
-      <div class="control">
-        <div class="columns">
-          <div  class="column">
-            مقدار پرداخت : 
-          </div>
-          <div class="control column is-two-thirds">
-            <input class="input" placeholder="مقدار پرداخت" v-model="deal.dealPayment.amount">
-          </div>
+      <div class="tile is-ancestor">
+        <div class="tile is-parent is-12">
+          <nav></nav>
         </div>
       </div>
-      <div class="contol">
-         <div class="columns">
-          <div  class="column">
-            تاریخ پرداخت: 
+      <div class="box">
+        <div class="control is-horizontal">
+          <p class="control">
+            <label class="radio">
+              <b-radio  v-model="deal.dealPayment.method" native-value="Cash" name="paymentType" />
+              نقدی
+            </label>
+            &nbsp;&nbsp;&nbsp;
+            <label class="radio">
+              <b-radio  v-model="deal.dealPayment.method" native-value="Cheque" name="paymentType" />
+              چک
+            </label>
+          </p>
+          <p class="control">
+            <label class="radio">
+              <vb-switch v-model="deal.dealPayment.payed" type="primary" ></vb-switch> &nbsp;
+              <span v-if="deal.dealPayment.payed">
+                پرداخت شده
+              </span>
+              <span v-else>
+                پرداخت نشده
+              </span>
+            </label>
+          </p>
+        </div>
+        <p>&nbsp;</p>
+        <div class="contol" v-if="deal.dealPayment.method == 'Cheque'">
+          <div class="columns">
+            <div  class="column">
+              سررسید چک :
+            </div>
+            <div class="control column is-two-thirds">
+              <date-picker type="date" :auto-submit="true" :editable="true" format="YYYY-MM-DD HH:mm" display-format="jYYYY/jMM/jDD" v-model="deal.dealPayment.dueDate"></date-picker>
+            </div>
           </div>
-          <div class="control column is-two-thirds">
-            <date-picker type="datetime" :auto-submit="true" :editable="true" format="YYYY-MM-DD HH:mm" display-format="HH:mm jYYYY/jMM/jDD" v-model="deal.dealPayment.payDate"></date-picker>
+        </div>
+        <p>&nbsp;</p>
+        <div class="control">
+          <div class="columns">
+            <div  class="column">
+              مقدار پرداخت : 
+            </div>
+            <div class="control column is-two-thirds">
+              <input class="input" placeholder="مقدار پرداخت" v-model="deal.dealPayment.amount">
+            </div>
           </div>
-         </div>
+        </div>
+        <div class="contol">
+          <div class="columns">
+            <div  class="column">
+              تاریخ پرداخت: 
+            </div>
+            <div class="control column is-two-thirds">
+              <date-picker type="datetime" :auto-submit="true" :editable="true" format="YYYY-MM-DD HH:mm" display-format="HH:mm jYYYY/jMM/jDD" v-model="deal.dealPayment.payDate"></date-picker>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </div>
